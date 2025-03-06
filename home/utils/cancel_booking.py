4e8769c05e4de5fcfff8c9602c1e7456ee876773
niteshctrl <nitesh1612@gmail.com>
2025-03-07 00:54:28 +0530

    Redesigned Pages for better experience

--- home/utils/cancel_booking.py
+++ home/utils/cancel_booking.py
@@ -4,9 +4,14 @@ from sqlalchemy import text
 def cancel(booking_id):
     engine = get_engine()
 
-    delete_query = text("DELETE FROM bookings WHERE index = :booking_id")
+    delete_query = text("DELETE FROM bookings WHERE booking_id = :booking_id")
+
+    inventory_increase = text(
+        "UPDATE inventory SET remaining_count = remaining_count + 1 where index= (SELECT inventory_id from bookings WHERE booking_id = :booking_id)"
+    )
 
     with engine.connect() as connection:
+        connection.execute(inventory_increase, {"booking_id": booking_id})
         connection.execute(delete_query, {"booking_id": booking_id})
         connection.commit() 
 
--- home/utils/make_booking.py
+++ home/utils/make_booking.py
@@ -1,16 +1,39 @@
-from .database_engine import get_engine
 from datetime import datetime
-from sqlalchemy import text
+from sqlalchemy import text, create_engine
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+def get_engine():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    load_dotenv(override=True, dotenv_path=str(BASE_DIR) + "/.env")
+
+    user = os.getenv('DB_USER')
+    password = os.getenv('DB_PASSWORD')
+    database = os.getenv('DB_NAME')
+    host = os.getenv('DB_HOST')
+    port = os.getenv('DB_PORT')
+
+    con = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+    engine = create_engine(con)
+    
+    return engine
 
 def make(member_id, inventory_id):
     engine = get_engine()
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    insert_query = text("INSERT INTO booking (member_id, datetime, inventory_id) VALUES (:member_id, :datetime, :inventory_id)"), {"member_id": member_id, "datetime": current_time, "inventory_id": inventory_id}
+    insert_query = text(
+        "INSERT INTO bookings (member_id, datetime, inventory_id) VALUES (:member_id, :datetime, :inventory_id)"
+    )
     
+    inventory_reduce = text(
+        "UPDATE inventory SET remaining_count = remaining_count - 1 where index= :inventory_id"
+    )
+
     with engine.connect() as connection:
-        connection.execute(insert_query)
+        connection.execute(insert_query, {"member_id": member_id, "datetime": current_time, "inventory_id": inventory_id})
+        connection.execute(inventory_reduce, {"inventory_id": inventory_id})
         connection.commit()
-
-
 
